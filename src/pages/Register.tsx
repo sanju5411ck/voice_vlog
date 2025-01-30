ce4228c4ae240b1b05f2b733a22b6b610265aab0
Sanjay <Sanjubanna5411@gmail.com>
2025-01-31 00:12:54 +0530

    image path in best way in worl

--- src/pages/Register.tsx
+++ src/pages/Register.tsx
@@ -30,7 +30,7 @@ export default function Register() {
     <div className="min-h-screen bg-gray-50 flex flex-col justify-center py-12 sm:px-6 lg:px-8">
       <div className="sm:mx-auto sm:w-full sm:max-w-md">
         <div className="flex justify-center">
-        <img src="/src/assets/img/logo.png" className="w-20 h-20 text-indigo-600" />
+        <img src="./logo.png" className="w-20 h-20 text-indigo-600" />
 
         </div>
         <h2 className="mt-6 text-center text-3xl font-extrabold text-gray-900">
